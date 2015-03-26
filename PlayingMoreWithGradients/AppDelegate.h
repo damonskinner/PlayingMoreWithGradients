a7ac27145433786b4cae77bfb50a5c8e9de411cb
damonskinner <dcs250@gmail.com>
2015-03-26 10:45:37 -0400

    first commit

---
+++ PlayingMoreWithGradients/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayingMoreWithGradients
+//
+//  Created by Damon Skinner on 3/25/15.
+//  Copyright (c) 2015 Damon Skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayingMoreWithGradients/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingMoreWithGradients
+//
+//  Created by Damon Skinner on 3/25/15.
+//  Copyright (c) 2015 Damon Skinner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
